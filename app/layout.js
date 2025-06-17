48766a54133e055d5e709e5fe85905f141ebeedf
abudaudhossain <abudaudhossainsumon@gmail.com>
2025-06-17 09:24:48 +0600

    :construction: add google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
+
 import "./globals.css";
+import Script from "next/script";
+import Analytics from "./components/Analytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,9 +26,29 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <div id="modal"></div>
-
+        <Analytics />
         {children}
       </body>
     </html>
